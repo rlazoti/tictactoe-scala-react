cf469fe596b00d4ff79ebc8593968ced67610953
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2016-06-12 20:50:04 -0300

    improve game result checking

--- tictacjoe-backend/src/main/scala/io/github/rlazoti/tictacjoe/backend/Board.scala
+++ tictacjoe-backend/src/main/scala/io/github/rlazoti/tictacjoe/backend/Board.scala
@@ -24,20 +24,63 @@ case class User(val name: String) extends Player {
 
 trait BoardState {
   def blanks: Int
-  def occupieds: Int
   def positions: Game.Positions
-  //def nextPlayer: Player
+
+  def over(player: Player, opponent: Player) =
+    won(player) || won(opponent) || draw(player, opponent)
+
+  def draw(player: Player, opponent: Player) =
+    !won(player) && !won(opponent) && blanks == 0
+
+  def won(player: Player) =
+    winningCol(player) || winningRow(player) || winningDiagonal(player) || winningReverseDiagonal(player)
+
+  private def checkValues(player: Player, values: Array[String]) =
+    values.foldLeft(true) { (acc, value) => acc && value == player.mark }
+
+  private def winningDiagonal(player: Player) = {
+    val values =
+      for (index <- 0 to Game.width - 1)
+      yield positions(index)(index)
+
+    checkValues(player, values.toArray)
+  }
+
+  private def winningReverseDiagonal(player: Player) = {
+    val values =
+      for (index <- 0 to Game.width - 1)
+      yield positions(Game.width - 1 - index)(index)
+
+    checkValues(player, values.toArray)
+  }
+
+  private def winningRow(player: Player) =
+    positions
+      .map { row => checkValues(player, row) }
+      .count { result => result == true } > 0
+
+  private def winningCol(player: Player) = {
+    def getColumn(columnIndex: Int) =
+      positions.map { row => row(columnIndex) }
+
+    val columns =
+      for (index <- 0 to Game.width - 1)
+      yield getColumn(index)
+
+    columns
+      .map { row => checkValues(player, row) }
+      .count { result => result == true } > 0
+  }
+
 }
 
 case class InitialState() extends BoardState {
   val blanks = Game.width * Game.width
-  val occupieds = blanks
   val positions = Array.fill(Game.width, Game.width)(Game.EmptyPosition)
 }
 
 case class NextState(currentState: BoardState, move: Move) extends BoardState {
   val blanks = currentState.blanks - 1
-  val occupieds = currentState.occupieds - 1
   val positions = buildPositions()
 
   private def buildPositions() = {
@@ -52,72 +95,49 @@ case class Move(val player: Player, val position: Position)
 case class Board(
   val id: Int,
   val width: Int = Game.width,
-  val cpu: Player = Computer(),
+  val opponent: Player = Computer(),
   val user: Player = User("Rodrigo"),
   val currentState: BoardState = InitialState()) {
 
+  private def isEnded() =
+    currentState.over(user, opponent)
+
+  private def userWon() =
+    currentState.won(user)
+
+  private def opponentWon() =
+    currentState.won(opponent)
+
+  private def draw() =
+    currentState.draw(user, opponent)
+
   def addMove(move: Move) = {
-    val b = Board(id, width, cpu, user, NextState(currentState, move))
-    println(s"next move => ${move.player.name} marks ${move.position}")
-
-    if (won(move.player)) println(s"${move.player.name} won!")
-
-    b
+    if (isEnded()) this
+    else {
+      val board = Board(id, width, opponent, user, NextState(currentState, move))
+      println(s"next move => ${move.player.name} marks ${move.position}")
+      board.printCurrentState()
+      board
+    }
   }
 
-  private def won(player: Player) =
-    winningCol(player) || winningRow(player) || winningDiagonal(player) || winningReverseDiagonal(player)
+  def printCurrentState() = {
+    val message =
+      if (userWon()) s"${user.name} won!"
+      else if (opponentWon()) s"${opponent.name} won!"
+      else if (draw()) s"Game Draw!"
+      else "Game is not over yet!"
 
-  private def winningDiagonal(player: Player) = {
-    def checkValues(values: Array[String]) =
-      values.foldLeft(true) { (acc, value) => acc && value == player.mark }
-
-    val values =
-      for (index <- 0 to Game.width - 1)
-      yield currentState.positions(index)(index)
-
-    checkValues(values.toArray)
-  }
-
-  private def winningReverseDiagonal(player: Player) = {
-    def checkValues(values: Array[String]) =
-      values.foldLeft(true) { (acc, value) => acc && value == player.mark }
-
-    val values =
-      for (row <- Game.width - 1 to 0; col <- 0 to Game.width - 1)
-      yield currentState.positions(row)(col)
-
-    checkValues(values.toArray)
-  }
-
-  private def winningRow(player: Player) = {
-    def checkRow(row: Array[String]) =
-      row.foldLeft(true) { (acc, value) => acc && value == player.mark }
-
-    currentState.positions.map(checkRow(_)).count { result => result } > 0
-  }
-
-  private def winningCol(player: Player) = {
-    def getCol(colIndex: Int) =
-      currentState.positions.map { row => row(colIndex) }
-
-    def checkCol(col: Array[String]) =
-      col.foldLeft(true) { (acc, value) => acc && value == player.mark }
-
-    val cols = for (index <- 0 to Game.width - 1) yield getCol(index)
-    cols.map(checkCol(_)).count { result => result } > 0
-  }
-
-  def printCurrentState() =
     println(s"""
-=============== Board =================
-  id: $id
-  blanks: ${currentState.blanks}
-  occupieds: ${currentState.occupieds}
-  positions: \n${currentState.positions.map(_.mkString("|"))mkString("\n")}
-=======================================
-""")
-
+      #=============== Board =================
+      #id: $id
+      #status: ${message}
+      #blanks: ${currentState.blanks}
+      #positions:
+      #${currentState.positions.map(_.mkString("|")).mkString("\n")}
+      #=======================================
+      #""".stripMargin('#'))
+  }
 }
 
 object Test extends App {
@@ -127,11 +147,11 @@ object Test extends App {
 
   val moves = List(
     Move(currentBoard.user, Position(0, 2)),
-    Move(currentBoard.cpu, Position(1, 0)),
+    Move(currentBoard.opponent, Position(1, 0)),
     Move(currentBoard.user, Position(2, 0)),
-    Move(currentBoard.cpu, Position(1, 2)),
+    Move(currentBoard.opponent, Position(1, 2)),
     Move(currentBoard.user, Position(1, 1)),
-    Move(currentBoard.cpu, Position(2, 2))
+    Move(currentBoard.opponent, Position(2, 2))
   )
 
   currentBoard.printCurrentState()
@@ -140,7 +160,7 @@ object Test extends App {
     boards = boards
       .filter { board => board.id == currentBoardId }
       .map { board => board.addMove(nextMove) }
-      .map { board => board.printCurrentState(); board }
+      //.map { board => board.printCurrentState(); board }
   }
 
 }
